3191faa2c0615c3a77742b020871447f0868b87d
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-02-04 23:05:19 +0300

    Update post-page.blade.php

--- resources/views/pages/portal/post-page.blade.php
+++ resources/views/pages/portal/post-page.blade.php
@@ -1,7 +1,7 @@
 <x-portal-layout>
     <div class="flex gap-8 justify-between">
         <livewire:components.article :post="$post"></livewire:components.article>
-        <livewire:components.posts-list :posts="$posts_list"></livewire:components.posts-list>
+        <livewire:components.posts-list posts_num="4" :posts="$posts_list"></livewire:components.posts-list>
     </div>
 
 </x-portal-layout>
